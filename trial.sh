3a15fba5b7baa50c484f6b5fa1ec694b53fecd15
pranavg1203 <pranavg2016@gmail.com>
2024-12-15 19:00:00 +0530

    minor

--- trial.sh
+++ trial.sh
@@ -4,13 +4,13 @@
 read -p "Enter the hostname for your container: " HOSTNAME
 
 # Define the image name (replace this with your Docker Hub image name)
-IMAGE_NAME="pranavg1203/meta2k24:trail_3"  # Replace with your Docker Hub username and image name
+IMAGE_NAME="pranavg1203/meta2k24:trail_4"  # Replace with your Docker Hub username and image name
 
 # Pull the Docker image from Docker Hub
 echo "Pulling the Docker image from Docker Hub..."
 docker pull "$IMAGE_NAME"
 
 # Run the container
-sudo docker run --hostname "$HOSTNAME" -it trail_2 /bin/bash -c "/home/ubuntu/flag_dir/script && /bin/bash"
+sudo docker run --hostname "$HOSTNAME" -it pranavg1203/meta2k24:trial_4 /bin/bash -c "/home/ubuntu/flag_dir/script && /bin/bash"
 
 
